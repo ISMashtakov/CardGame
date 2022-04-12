ec1df456b718b56b12798da955a7054b8a938d9c
imashtak <mashtakovigor@mail.ru>
2022-04-12 15:39:42 +0300

    edit cards decoding

--- Assets/Scripts/Cards/CardDecoder.cs
+++ Assets/Scripts/Cards/CardDecoder.cs
@@ -1,14 +1,21 @@
 using CardGame.Web.Messages;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace CardGame.Game.Cards {
     public static class CardDecoder
     {
+        static IEnumerable<Type> types = Assembly.GetAssembly(typeof(Card)).GetTypes().Where(type => type.IsSubclassOf(typeof(Card)));
         public static Card Decode(PlayedCardMessage playedCardMessage)
         {
-            if (playedCardMessage.cardTypeName == typeof(MaceHit).FullName)
+            foreach (Type type in types)
             {
-                return MaceHit.FromString(playedCardMessage.data);
+                if (playedCardMessage.cardTypeName == type.FullName)
+                {
+                    return (Card)type.GetMethod("FromString").Invoke(null, new string[] { playedCardMessage.data });
+                }
             }
 
             throw new NotImplementedException($"Decoding for {playedCardMessage.cardTypeName} not implemented");
