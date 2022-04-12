b65392e7e7eb0798966fb68133a32d6dfcfb4510
imashtak <mashtakovigor@mail.ru>
2022-04-12 13:10:57 +0300

    add main menu

--- Assets/Scripts/MainMenu/MenuController.cs
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -1,19 +1,58 @@
 using CardGame.Web;
-using CardGame.Web.Messages;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using Network = CardGame.Web.Network;
 
 namespace CardGame.MainMenu
 {
     public class MenuController : MonoSingletone<MenuController>
     {
-        private void Start()
+        [SerializeField] ConnectionManager _connectionManager;
+
+        [SerializeField] GameObject _mainMenu;
+        [SerializeField] GameObject _waitingCall;
+        [SerializeField] GameObject _calling;
+
+        MenuController()
         {
-            Network.GetInstanse().OnConnectListener.AddListener(AllConnected);
+            Network.GetInstanse().OnConnectListener.AddListener(StartGame);
         }
 
-        void AllConnected()
+        public void OnClickWaitCall()
         {
-            UnityEngine.Debug.Log("ALl COnnected");
+            _connectionManager.StartServer();
+            _mainMenu.SetActive(false);
+            _waitingCall.SetActive(true);
+        }
+
+        public void OnClickCall()
+        {
+            _connectionManager.StartClient();
+            _mainMenu.SetActive(false);
+            _calling.SetActive(true);
+        }
+
+        public void OnClickCancelWaitingCall()
+        {
+            _connectionManager.StopServer();
+            _waitingCall.SetActive(false);
+            _mainMenu.SetActive(true);
+        }
+
+        public void OnClickCancelCalling()
+        {
+            _connectionManager.StopClient();
+            _calling.SetActive(false);
+            _mainMenu.SetActive(true);
+        }
+
+        public void OnClickExit()
+        {
+            Application.Quit();
+        }
+
+        void StartGame()
+        {
             SceneManager.LoadScene(1);
         }
     }
